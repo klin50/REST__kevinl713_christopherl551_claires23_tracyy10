c824c33ee5ae4cbc3b24a9e1983a296f63960005
Claire <csong50@stuy.edu>
2024-12-01 13:11:21 -0500

    created tables for db

--- app/database.py
+++ app/database.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+db = "rest.db"
+
+database = sqlite3.connect(db)
+c = database.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, userID INTEGER PRIMARY KEY AUTOINCREMENT, points INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS cards(imgLink TEXT, advice BOOLEAN, insult BOOLEAN, quote TEXT, userID INTEGER, FOREIGN KEY (userID) REFERENCES users(userID))")
+c.execute("CREATE TABLE IF NOT EXISTS used(advice BOOLEAN, insult BOOLEAN, img BOOLEAN, propertyID TEXT)")
