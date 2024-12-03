4395c03886b8bfc3583561d785a6aef3c5e03036
Claire Song <csong50@stuy.edu>
2024-12-03 11:59:00 -0500

    updated tables and functions for tables

--- app/__init__.py
+++ app/__init__.py
@@ -11,14 +11,46 @@ import API
 import database
 
 app = Flask(__name__)
+secret = os.urandom(32)
+app.secret_key = secret
+
+database.build()
 
 @app.route("/")
 def home():
-    return render_template("home.html", x = "weeee")
-def login():
-    return render_template("login.html", x = "weeee")
+    if 'username' in session: #Checks if logged in
+        logged = True
+        uname = session['username']  #Displays username
+        logged = False
+    return render_template("home.html",logged=logged,uname=uname)
+
+@app.route("/login")
+def disp_loginpage():
+    return render_template('login.html') #Login Page Rendering
+
+@app.route("/auth", methods=['POST'])
+def authenticate():#Is called when the user tries to log into their account
+    username = request.form.get('username')
+    password = request.form.get('password')
+    stored_password = getPass(username)
+    if stored_password and stored_password[0] == password:
+        session['username'] = username
+        info = c.execute("SELECT * FROM users WHERE username = ?", username).fetchone()
+        session['userID'] = info['userID']
+        return redirect("/")
+    return render_template('login.html', error="Invalid username or password")
+
+@app.route("/createAccount")
+def disp_register():
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
 def register():
-    return render_template("register.html", x = "weeee")
+    username = request.form.get('username')
+    password = request.form.get('password')
+    c.execute("INSERT INTO users VALUES(?, ?, ?)", (username, password, 0))
+    return render_template("register.html")
+
 def selectD():
     return render_template("selectD.html", x = "weeee")
 def question():
@@ -26,9 +58,13 @@ def question():
 def gacha():
     return render_template("gacha.html", x = "weeee")
 def collection():
+    c.execute("SELECT * FROM cards WHERE userID = ?", session['userID'])
     return render_template("collection.html", x = "weeee")
+
+@app.route("/logout")
 def logout():
-    return redirect(url_for("/"))
+    session.pop('username', None)
+    return render_template('logout.html')#Similar to login page, just removes session
 
 if __name__ == "__main__":
     app.debug = True
--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
 import sqlite3
 
-db = "rest.db"
+def build():
+    database = sqlite3.connect("rest.db")
+    c = database.cursor()
 
-database = sqlite3.connect(db)
-c = database.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, points INTEGER, userID INTEGER PRIMARY KEY AUTOINCREMENT)")
+    c.execute("CREATE TABLE IF NOT EXISTS cards(imgLink TEXT, advice BOOLEAN, insult BOOLEAN, quote TEXT, userID INTEGER, FOREIGN KEY (userID) REFERENCES users(userID))")
+    c.execute("CREATE TABLE IF NOT EXISTS used(advice BOOLEAN, insult BOOLEAN, image BOOLEAN, question BOOLEAN, propertyID TEXT)")
 
-c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, userID INTEGER PRIMARY KEY AUTOINCREMENT, points INTEGER)")
-c.execute("CREATE TABLE IF NOT EXISTS cards(imgLink TEXT, advice BOOLEAN, insult BOOLEAN, quote TEXT, userID INTEGER, FOREIGN KEY (userID) REFERENCES users(userID))")
-c.execute("CREATE TABLE IF NOT EXISTS used(advice BOOLEAN, insult BOOLEAN, img BOOLEAN, propertyID TEXT)")
+    database.commit()
+    database.close()
