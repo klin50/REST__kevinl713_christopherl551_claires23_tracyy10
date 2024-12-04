adadbc455c0e4115087400e541a959104c795caf
Claire <csong50@stuy.edu>
2024-12-03 23:12:59 -0500

    finished login and register methods

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask import session
 from flask import redirect
 from flask import url_for
 from flask import flash
+import os
+import sqlite3
 # ---
 import API
 import database
@@ -16,40 +18,54 @@ app.secret_key = secret
 
 database.build()
 
+def connect():
+    db = sqlite3.connect("rest.db")
+    c = db.cursor()
+    return c, db
+
+def close():
+    db.commit()
+    db.close()
+
 @app.route("/")
 def home():
     if 'username' in session: #Checks if logged in
         logged = True
         uname = session['username']  #Displays username
-        logged = False
-    return render_template("home.html",logged=logged,uname=uname)
+        return render_template("home.html",logged=logged,uname=uname)
+    logged = False
+    return render_template("home.html",logged=logged)
 
 @app.route("/login")
 def disp_loginpage():
     return render_template('login.html') #Login Page Rendering
 
 @app.route("/auth", methods=['POST'])
-def authenticate():#Is called when the user tries to log into their account
+def authenticate():#Is called when the user enters their username & password into the form
     username = request.form.get('username')
     password = request.form.get('password')
-    stored_password = getPass(username)
-    if stored_password and stored_password[0] == password:
+    c, restdb = connect()
+    info = c.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone() #Finds user's row based on the entered username
+    restdb.close()
+    stored_password = info[1] #Gets user's password from database
+    if stored_password and stored_password[0] == password: #If password is correct
         session['username'] = username
-        info = c.execute("SELECT * FROM users WHERE username = ?", username).fetchone()
-        session['userID'] = info['userID']
+        session['userID'] = info[3] #Based on userID in database
         return redirect("/")
     return render_template('login.html', error="Invalid username or password")
 
 @app.route("/createAccount")
-def disp_register():
+def disp_register(): #Register Page Rendering
     return render_template('register.html')
 
 @app.route("/register", methods=['POST'])
-def register():
+def register(): #Is called when user enters their username and password into the form
     username = request.form.get('username')
     password = request.form.get('password')
-    c.execute("INSERT INTO users VALUES(?, ?, ?)", (username, password, 0))
-    return render_template("register.html")
+    c, restdb = connect()
+    c.execute("INSERT INTO users(username, password, points) VALUES(?, ?, ?)", (username, password, 0))
+    restdb.close()
+    return redirect("/login")
 
 def selectD():
     return render_template("selectD.html", x = "weeee")
@@ -58,7 +74,9 @@ def question():
 def gacha():
     return render_template("gacha.html", x = "weeee")
 def collection():
+    c, restdb = connect()
     c.execute("SELECT * FROM cards WHERE userID = ?", session['userID'])
+    restdb.close()
     return render_template("collection.html", x = "weeee")
 
 @app.route("/logout")
