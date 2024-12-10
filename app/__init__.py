cb6479ee862b6e2d7fbf8b2181df6d47b38ec1c4
Tracy Ye <tye50@stuy.edu>
2024-12-10 03:35:51 -0500

    trivia&results are done. devlog updated.

--- app/__init__.py
+++ app/__init__.py
@@ -65,6 +65,28 @@ def register(): #Is called when user enters their username and password into the
         flash("Username already exists")
         return redirect("/createAccount")
 
+# what is this func????
+''' 
+@app.route("/checkcorrect",methods=['GET','POST'])
+def auth(): # what in the world is this func??
+    question = request.form.get('question')
+    answer = request.form.get('answer')
+    info = database.auth(answer)
+    if info != None:
+        stored_password = info[1] #Gets user's password from database
+        if stored_password == password: #If password is correct
+            session['username'] = username
+            session['userID'] = info[3] #Based on userID in database
+            #print(info)
+            #print(info[3])
+            #print(session['userID'])
+            #print("Both")
+            return redirect("/")
+        return redirect("/incorrect")
+    flash("Error")
+    return redirect("/question")
+'''
+
 @app.route("/selection", methods=['GET','POST'])
 def selectD():
     if request.method == "GET":
@@ -75,7 +97,7 @@ def selectD():
         if qType == "none":
             trivia = API.genTrivia()
         elif qType in ["easy", "medium", "hard"]:
-            trivia = API.genTriviaDifficulty()
+            trivia = API.genTriviaDifficulty(qType)
         elif qType in ["music", "sport_and_leisure", "film_and_tv", "arts_and_literature", "history", "society_and_culture", "science", "geography", "food_and_drink", "general_knowledge"]:
             trivia = API.genTriviaCategory(qType)
         question = API.getQuestion(trivia)
@@ -94,58 +116,41 @@ def selectD():
         iTC = []
         for i in answers:
             if i == correctA:
-                iTC.append("yes")
+                iTC.append("correct")
             else:
-                iTC.append("no")
+                iTC.append("incorrect")
         return render_template("question.html", isThisCorrect = iTC, A = answers, bgColor = bg, D = difficulty, C = category, Q = question)
     else:
+        return render_template("questions.html") # wont happen
 
-        return render_template("questions.html")
-
-@app.route("/checkcorrect",methods=['GET','POST'])
-    question = request.form.get('question')
-    answer = request.form.get('answer')
-    info = database.auth(answer)
-    if info != None:
-        stored_password = info[1] #Gets user's password from database
-        if stored_password == password: #If password is correct
-            session['username'] = username
-            session['userID'] = info[3] #Based on userID in database
-            #print(info)
-            #print(info[3])
-            #print(session['userID'])
-            #print("Both")
-            return redirect("/")
-        return redirect("/incorrect")
-    flash("Error")
-    return redirect("/question")
-
-@app.route("/correct",methods=['GET','POST'])
-def correctA():
-    #addPoints(session['username']) #FUNCTION NEEDS WRITING
-    return render_template("correct.html") # STILL NEEDS TO BE WRITTEN
-
-@app.route("/incorrect",methods=['GET','POST'])
-def incorrectA():
-    return render_template("incorrect.html") #STILL NEEDS TO BE WRITTEN
-
-@app.route("/question", methods=['GET','POST'])
-def question():
-    if request.method == "GET":
-        return redirect("/question") # this shouldnt be happening, pretty sure it'll crash if it ever runs
-    elif request.method == "POST":
-        return render_template("selection.html")
+@app.route("/answer", methods=['GET','POST'])
+def retAnswer():
+    if request.method == "POST":
+        result = request.form.get("ans")
+        bg = "ugh"
+        insult = "ugh"
+        quote = "ugh"
+        ID = "ugh"
+        if result == "correct":
+            bg = "emerald-200"
+        else:
+            bg = "red-400"
+            insult = API.genInsult()
+            quote = API.getInsult(insult)
+            ID = API.getInsultID(insult)
+        # now give points etc, etc
+        return render_template("answer.html", R = result, I = quote, bgColor = bg, x = request.form.get("ans"))
+    return redirect("/")
 
 @app.route("/gacha", methods=['GET','POST'])
 def gacha():
-#     cat = API.genCat()
-#     slip = API.genAdvice()
-#     advice = API.getAdvice(slip)
-#     #database.checkUsed()
-#     print(session['userID'])
-#     database.addCard(cat, advice, session['userID'])
-#     return render_template("gacha.html", img1 = API.getCat(cat), x = request.form.get("isThisCorrect"))
-    return render_template("gacha.html", x = request.form.get("isThisCorrect"))
+    cat = API.genCat()
+    slip = API.genAdvice()
+    advice = API.getAdvice(slip)
+    #database.checkUsed()
+    print(session['userID'])
+    database.addCard(cat, advice, session['userID'])
+    return render_template("gacha.html", img1 = API.getCat(cat), x = request.form.get("isThisCorrect"))
 
 @app.route("/collection")
 def collection():
